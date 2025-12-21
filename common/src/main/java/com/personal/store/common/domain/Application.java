648fd12d48eff8d3d4aa2f1f6ff49199b6a54ccf
Bruno Lima <brunojet@gmail.com>
2025-12-21 19:25:44 -0300

    Refactor

--- common/src/main/java/com/personal/store/common/domain/Application.java
+++ common/src/main/java/com/personal/store/common/domain/Application.java
@@ -1,11 +1,18 @@
 package com.personal.store.common.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.personal.store.common.domain.abstraction.SimpleEntityAbstraction;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "application")
 public class Application extends SimpleEntityAbstraction {
+	@OneToMany(mappedBy = "application", fetch = FetchType.LAZY)
+	private final List<ApplicationImage> images = new ArrayList<>();
 }
--- common/src/main/java/com/personal/store/common/domain/ApplicationImage.java
+++ common/src/main/java/com/personal/store/common/domain/ApplicationImage.java
@@ -5,6 +5,9 @@ import com.personal.store.common.domain.abstraction.StorageObjectAbstraction;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,13 +15,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "image")
+@Table(name = "application_image")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicationImage extends StorageObjectAbstraction {
-    @Column(name = "application_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "application_id", nullable = false)
+    private Application application;
+
+    @Column(name = "application_id", nullable = false, insertable = false, updatable = false)
     private Long applicationId;
 
     @Column(name = "image_type")
