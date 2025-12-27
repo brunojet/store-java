1eaa2ae2badae14e69cc29b40c6ff757d39eeff0
Bruno Lima <brunojet@gmail.com>
2025-12-27 20:19:30 -0300

    test

--- common/src/main/java/com/personal/store/common/domain/ApplicationCatalog.java
+++ common/src/main/java/com/personal/store/common/domain/ApplicationCatalog.java
@@ -1,9 +1,12 @@
 package com.personal.store.common.domain;
 
+import com.personal.store.common.domain.abstraction.AuditAbstraction;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinColumns;
 import jakarta.persistence.MapsId;
@@ -15,26 +18,25 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "application_catalog")
+@Table(name = "application_catalog", indexes = {
+        @Index(name = "ix_app_catalog_app_term_stage", columnList = "application_id, terminal_configuration_id, stage"),
+        @Index(name = "ix_app_catalog_stage_term_app", columnList = "stage, terminal_configuration_id, application_id"),
+        @Index(name = "ix_app_catalog_deleted_created", columnList = "deleted, created"),
+        @Index(name = "ix_app_catalog_updated_created", columnList = "updated, created")
+})
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApplicationCatalog {
+public class ApplicationCatalog extends AuditAbstraction {
     @EmbeddedId
     private ApplicationCatalogId id = new ApplicationCatalogId();
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @MapsId("applicationConfigurationId")
     @JoinColumns({
-        @JoinColumn(
-            name = "application_id",
-            referencedColumnName = "application_id"
-        ),
-        @JoinColumn(
-            name = "terminal_configuration_id",
-            referencedColumnName = "terminal_configuration_id"
-        )
+            @JoinColumn(name = "application_id", referencedColumnName = "application_id"),
+            @JoinColumn(name = "terminal_configuration_id", referencedColumnName = "terminal_configuration_id")
     })
     private ApplicationConfiguration applicationConfiguration;
 
