ad4ac319f8f715812be87064fa7de3d2083b0cd2
Bruno Lima <brunojet@gmail.com>
2025-12-27 21:03:54 -0300

    Refactor

--- common/src/main/java/com/personal/store/common/domain/ApplicationProfileScreenshot.java
+++ common/src/main/java/com/personal/store/common/domain/ApplicationProfileScreenshot.java
@@ -1,40 +1,37 @@
 package com.personal.store.common.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "application_profile_screenshot")
-@IdClass(ApplicationProfileScreenshotId.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicationProfileScreenshot {
-    @Id
-    @Column(name = "application_profile_id")
-    private Long applicationProfileId;
-
-    @Id
-    @Column(name = "application_image_id")
-    private Long applicationImageId;
+    @EmbeddedId
+    private ApplicationProfileScreenshotId id = new ApplicationProfileScreenshotId();
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "application_profile_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @MapsId("applicationProfileId")
+    @JoinColumn(name = "application_profile_id", referencedColumnName = "id", nullable = false)
     private ApplicationProfile applicationProfile;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "application_image_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @MapsId("applicationImageId")
+    @JoinColumn(name = "application_image_id", referencedColumnName = "id", nullable = false)
     private ApplicationImage applicationImage;
 
     @Column(name = "position", nullable = false)
